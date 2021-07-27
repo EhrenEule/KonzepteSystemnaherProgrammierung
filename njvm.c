730a8a837d31b955a27167f2d23b310709b8a25f
EhrenEule <leonardvonschneider@gmail.com>
2021-07-27 16:04:33 +0200

    Task 0 finished

--- njvm.c
+++ njvm.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#define VERSION 0.1
+
 int main(int argc, char *argv[]) {
-    printf("Hello, world!\n");
+
+    for(int i=0; i< argc; i++)
+    {
+        if(strcmp(argv[i], "--version"))
+        {
+            printf("Version: %c",VERSION);
+        }
+        else if(strcmp(argv[i], "--help"))
+        {
+            printf("usage: ./njvm [option] [option] ...\n");
+            printf("  --version        show version and exit\n");
+            printf("  --help           show this help and exit\n");
+        }
+        else
+        {
+            printf("unknown command line argument '%c', try './njvm --help'\n", argv[i]);
+        }
+    }
     return 0;
 }
