c3cfa8de3e6246efc49bdd072db5ec511b274ba9
EhrenEule <leonardvonschneider@gmail.com>
2021-08-06 10:24:25 +0200

    Finished first part of Task 3

--- njvm.h
+++ njvm.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-#define VERSION 2
+#define VERSION 3
 
 #define HALT 0
 #define PUSHC 1
@@ -15,21 +15,21 @@
 #define RDINT 7
 #define WRINT 8
 #define RDCHR 9
-#define WRCHR 10
-#define PUSHG 11
-#define POPG 12
+#define WRCHR 10 // a
+#define PUSHG 11 // b
+#define POPG 12 // c
 #define ASF 13
 #define RSF 14
 #define PUSHL 15
 #define POPL 16
 #define EQ 17
-#define NE 18
+#define NE 18 // 12
 #define LT 19
 #define LE 20
-#define GT 21
+#define GT 21 // 15
 #define GE 22
-#define JMP 23
-#define BRF 24
+#define JMP 23 // 17
+#define BRF 24 // 18
 #define BRT 25
 
 #define IMMEDIATE(x) ((x) & 0x00FFFFFF)
